d671ca0458ea9a38a3713ab9c940f3199b145529
george <gpsavill+gitlabgeorge@gmail.com>
2020-08-03 16:01:47 +0100

    Add service to return image list

---
+++ photography-gallery/Services/ImageListService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace photography_gallery.Services
+{
+    public class ImageListService
+    {
+        public ImageListService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        public string GetImagesDirectory()
+        {
+            return Path.Combine(WebHostEnvironment.WebRootPath, "images");
+        }
+    }
+}
--- photography-gallery/Startup.cs
+++ photography-gallery/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using photography_gallery.Services;
 
 namespace photography_gallery
 {
@@ -24,6 +25,7 @@ namespace photography_gallery
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddTransient<ImageListService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
