b308ee83c74861a7552df01faf72b44aeaa87f5d
georgesavill <gpsavill+github@gmail.com>
2020-08-07 16:21:21 +0100

    Add basic file upload and resize functionality

--- photography-gallery/Services/ImageUpload.cs
+++ photography-gallery/Services/ImageUpload.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
 
 namespace photography_gallery.Services
 {
@@ -14,8 +16,14 @@ namespace photography_gallery.Services
         [HttpPost]
         public IActionResult Upload(IFormFile file)
         {
-            Console.WriteLine("Photo uploaded:");
-            Console.WriteLine(file.FileName);
+            using var image = Image.Load(file.OpenReadStream());
+
+            image.Mutate(x => x.Resize(315, 315));
+
+            string uploadedImageName = file.FileName.Split(".").First();
+            string uploadedImageExtension = file.FileName.Split(".").Last();
+            image.Save("./wwwroot/images/uploaded/" + uploadedImageName + "_thumbnail." + uploadedImageExtension);
+
             return Ok();
         }
     }
