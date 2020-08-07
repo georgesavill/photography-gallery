0f423a96f8917a2d19c74f46999290f8e7432257
georgesavill <gpsavill+github@gmail.com>
2020-08-07 15:47:51 +0100

    Add image upload API stub

---
+++ photography-gallery/Services/ImageUpload.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace photography_gallery.Services
+{
+    [ApiController]
+    [Route("/uploadImages")]
+    public class ImageUpload : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Upload(IFormFile file)
+        {
+            Console.WriteLine("Photo uploaded:");
+            Console.WriteLine(file.FileName);
+            return Ok();
+        }
+    }
+}
